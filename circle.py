aed44afdf47bc3a9e32f353b9b5c3dd1e60ab0e1
Shinewit <112455198+Shinewit@users.noreply.github.com>
2023-10-08 21:29:58 +0800

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,14 @@
+from turtle import *
+speed(15)
+
+penup()
+backward(50)
+pendown()
+
+pencolor('darkblue'),pensize(3),circle(50),penup(),forward(70),pendown()
+pencolor('black'),circle(50),penup(),forward(70),pendown()
+pencolor('red'),circle(50),penup()
+left(90),forward(50),left(90),forward(25),pendown()
+pencolor('green'),circle(50),penup(),forward(70),pendown()
+pencolor('orange'),circle(50)
+done()
